7a643761597478ac0725d8913163ac98f542bfa7
oscar lodriguez <o.lodriguez@gmail.com>
2019-11-21 15:36:47 +0100

    rewrite

---
+++ main.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: oscar
+-- Date: 21-11-19
+-- Time: 15:32
+-- To change this template use File | Settings | File Templates.
+--
+
+function love.draw()
+    love.graphics.print("Hello World", 400, 300)
+end
