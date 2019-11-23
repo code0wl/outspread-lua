5de58a11aa2906d76d4fa0f92ea81671f701adc7
oscar lodriguez <o.lodriguez@gmail.com>
2019-11-23 04:30:27 +0100

    sample done

--- click-game/main.lua
+++ click-game/main.lua
@@ -9,30 +9,55 @@ function love.load()
     button.size = 50
 
     score = 0
-    timer = 0
+    timer = 10
+
+    gameState = 1
 
     myFont = love.graphics.newFont(50)
 end
 
 function love.update(dt)
+    if gameState == 2 then
+        if timer > 0 then
+            timer = timer - dt
+        end
+
+        if timer < 0 then
+            timer = 0
+            gameState = 1
+
+        end
+    end
 end
 
 function love.draw()
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", button.x, button.y, button.size)
+    if gameState == 2 then
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.circle("fill", button.x, button.y, button.size)
+    end
 
     love.graphics.setFont(myFont)
     love.graphics.setColor(1, 1, 1)
     love.graphics.print(score)
+    love.graphics.print(math.ceil(timer), love.graphics.getWidth() / 2)
+
+    if gameState == 1 then
+        love.graphics.print('click anywhere to start', 100, 300)
+    end
 end
 
 function love.mousepressed(x, y, b, istouch)
-    if b == 1 then
+    if b == 1 and gameState == 2 then
         if distanceBetween(x, y, button.x, button.y) - button.size < 0 then
             score = score + 1
 
             button.x = math.random(button.size, love.graphics.getWidth() - button.size)
-            button. y = math.random(button.size, love.graphics.getHeight() - button.size)
+            button.y = math.random(button.size, love.graphics.getHeight() - button.size)
         end
+    end
+    if gameState == 1 then
+        gameState = 2
+        timer = 10
+        score = 0
     end
 end
