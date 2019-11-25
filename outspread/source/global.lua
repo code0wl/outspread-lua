5561484b60ae133e1d64e281c9b8f7f5a9b879cb
oscar lodriguez <o.lodriguez@gmail.com>
2019-11-25 07:55:51 +0100

    tiny include

--- outspread/source/global.lua
+++ outspread/source/global.lua
@@ -10,6 +10,21 @@ sti = require("libs/sti")
 anim8 = require('libs/anim8')
 tiny = require('libs/tiny/system')
 talkingSystem = tiny.processingSystem()
+
+talkingSystem.filter = tiny.requireAll("name", "mass", "phrase")
+
+function talkingSystem:process(e, dt)
+    e.mass = e.mass + dt * 3
+    print(("%s who weighs %d pounds, says %q."):format(e.name, e.mass, e.phrase))
+end
+
+local joe = {
+    name = "Joe",
+    phrase = "I'm a plumber.",
+    mass = 150,
+    hairColor = "brown"
+}
+
 world = tiny.world(talkingSystem, joe)
 
 -- create instance for camera
