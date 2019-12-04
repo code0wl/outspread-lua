a4511d88fa2b0b736e880e04502cf5c52ef9e2a6
oscar lodriguez <o.lodriguez@gmail.com>
2019-12-04 15:55:38 +0100

    rock

--- src/entities/Rock.lua
+++ src/entities/Rock.lua
@@ -1,19 +1,17 @@
 local Rock = {}
 
-function Rock(rockConfig)
-    local rock = {}
-
+function Rock:new(rockConfig)
+    local rock = setmetatable({}, {__index = Rock})
     rock.width = rockConfig.width
     rock.height = rockConfig.height
     rock.x = rockConfig.x
     rock.y = rockConfig.y
-    function rock.draw ()
-        lg.setColor(255, 153, 153)
-        lg.rectangle("fill", rock.x, rock.y, rock.width, rock.height)
-    end
-
     return rock
+end
 
+function Rock:draw()
+    lg.setColor(255, 153, 153)
+    lg.rectangle("fill", self.x, self.y, self.width, self.height)
 end
 
 return Rock
