70f44026e8183c7908322ce0f09e4230a69f1207
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-12-29 18:00:36 +0100

    removed submodule

--- src/global.lua
+++ src/global.lua
@@ -10,7 +10,7 @@ sti = require("libs/sti")
 anim8 = require('libs/anim8')
 inspect = require("libs/inspect/inspect")
 class = require("libs.middleclass")
-local lovetoys = require("libs.lovetoys")
+local lovetoys = require("libs._lovetoys")
 lovetoys.initialize({globals = true})
 
 -- collision
