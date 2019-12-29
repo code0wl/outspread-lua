d5ec1d4715cad7b38d060d4617d4c8975d3a4b71
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-12-29 18:26:49 +0100

    state

--- src/main.lua
+++ src/main.lua
@@ -7,6 +7,8 @@ local asset = require("Assets")
 local OutSpreadEngine = require("OutSpreadEngine")
 local Control = require("Control")
 
+local gameState = 1
+
 local bg_image = Lg.newImage("/images/background/background.png")
 
 bg_image:setWrap("repeat", "repeat")
@@ -30,22 +32,36 @@ function love.draw()
     local mouseX, mouseY = Lm.getPosition()
     local currentX, currentY = Cam:getPosition()
 
-    -- Camera
-    Cam:draw(function(l, t, w, h)
+    if gameState == 1 then
+        -- Camera
+        Cam:draw(function(l, t, w, h)
 
-        Lg.draw(bg_image, QuadBQ, 0, 0)
+            Lg.draw(bg_image, QuadBQ, 0, 0)
 
-        -- Draw player phermones
-        for _, phermone in ipairs(Player.phermones) do
-            Lg.setColor(255, 153, 153)
-            Lg.circle('fill', phermone.x, phermone.y, 5)
-        end
+            -- Draw player phermones
+            for _, phermone in ipairs(Player.phermones) do
+                Lg.setColor(255, 153, 153)
+                Lg.circle('fill', phermone.x, phermone.y, 5)
+            end
 
-        engine:draw()
+            engine:draw()
 
-        UpdateCameraLocation(mouseX, mouseY, currentX, currentY)
+            UpdateCameraLocation(mouseX, mouseY, currentX, currentY)
 
-    end)
+        end)
+
+    end
+
+    if gameState == 2 then
+        -- Camera
+        Cam:draw(function(l, t, w, h) print("some other shit") end)
+
+    end
+
+    if gameState == 3 then
+        -- Camera
+        Cam:draw(function(l, t, w, h) print("some other shit") end)
+    end
 
     suit.draw()
 
