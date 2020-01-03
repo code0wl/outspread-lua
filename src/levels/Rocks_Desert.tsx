23f7a215d322685561c484cba5d8e94031d35e81
Oscar Lodriguez <o.lodriguez@gmail.com>
2020-01-03 17:13:22 +0100

    images

---
+++ src/levels/Rocks_Desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Rocks_Desert" tilewidth="64" tileheight="64" tilecount="224" columns="14">
+ <image source="../../../../../Downloads/223b0a629c80464e2a4da29b4b315a33/Rocks_Desert.png" width="916" height="1078"/>
+</tileset>
