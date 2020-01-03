c872e49cd39eccfcad3759a4285a09f628037ef1
Oscar Lodriguez <o.lodriguez@gmail.com>
2020-01-03 20:38:15 +0100

    poop

--- src/PrepareImages.lua
+++ src/PrepareImages.lua
@@ -2,5 +2,5 @@ BlackWalk = Lg.newImage("images/ants/spritesheets/ant1/_ant_walk-small.png")
 RedWalk = Lg.newImage("images/ants/spritesheets/ant2/_ant_walk-small.png")
 WalkingTarantula = Lg.newImage(
                        "images/spiders/spider1/spritesheets/sheet_spider_walk-small.png")
-Grass = Lg.newImage("images/tiles/Dirt_Grass.png")
+Grass = Lg.newImage("images/tiles/dirt_grass_small.png")
 
--- src/Worldmap.lua
+++ src/Worldmap.lua
@@ -4,31 +4,28 @@ function WorldMap:initialize(worldMapConfig) self.tilesToRender = {} end
 
 function WorldMap:draw()
 
-    block_width = Grass:getWidth()
-    block_height = Grass:getHeight()
-    block_depth = block_height / 2
+    local block_width = Grass:getWidth()
+    local block_height = Grass:getHeight()
+    local block_depth = block_height / 2
 
-    x = 0
-    y = 0
-    grid_x = 0
-    grid_y = 0
-    grid_size = 20
-    grid = {}
+    local x = 0
+    local y = 0
+    local grid_x = 0
+    local grid_y = 0
+    local grid_size = 10
+    local grid = {}
+
     for x = 1, grid_size do
         grid[x] = {}
         for y = 1, grid_size do grid[x][y] = 1 end
     end
 
-    grid[2][4] = 2
-    grid[6][5] = 2
-
     for x = 1, grid_size do
         for y = 1, grid_size do
             if grid[x][y] == 1 then
-                love.graphics.draw(Grass,
-                                   grid_x + ((y - x) * (block_width / 2)),
-                                   grid_y + ((x + y) * (block_depth / 2)) -
-                                       (block_depth * (grid_size / 2)))
+                Lg.draw(Grass, grid_x + ((y - x) * (block_width / 2)), grid_y +
+                            ((x + y) * (block_depth / 2)) -
+                            (block_depth * (grid_size / 2)))
 
             end
         end
