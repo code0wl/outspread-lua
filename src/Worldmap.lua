ee5681058848ab1fb1b6dba8fbdb4fc4b9d6a9cf
oscar lodriguez <o.lodriguez@gmail.com>
2020-01-17 16:52:37 +0100

    refactor worlds

--- src/Worldmap.lua
+++ src/Worldmap.lua
@@ -4,14 +4,15 @@ function WorldMap:draw()
     local blockWidth = 300 / 2
     local blockHeight = 300 / 2
     local block_depth = blockHeight / 2.25
-    local level = 0
+    local levels = {}
+    local i = 0
     local grid_size = 5
     local graphic = nil
     local mouseX, mouseY = Lm.getPosition()
 
     for x = 1, grid_size do
         for y = 1, grid_size do
-            level = level + 1
+            i = i + 1
             local deltaX, deltaY =
                 ((y - x) * (util.getCenter(blockWidth))),
                 ((x + y) * (util.getCenter(block_depth))) - (block_depth * util.getCenter(grid_size))
@@ -20,15 +21,24 @@ function WorldMap:draw()
                 util.getCenter(Lg.getWidth()) / grid_size + deltaX,
                 util.getCenter(Lg.getHeight()) / grid_size + deltaY
 
-            if level > 10 and level < 15 then
-                graphic = {image = SomeGrass, id = 1}
-            elseif level > 15 and level < 20 then
-                graphic = {image = Sand, id = 2}
-            elseif level == 25 then
-                graphic = {image = Rock, id = 3}
+            if i > 10 and i < 15 then
+                graphic = SomeGrass
+            elseif i > 15 and i < 20 then
+                graphic = Sand
+            elseif i == 25 then
+                graphic = Rock
             else
-                graphic = {image = Grass, id = 4}
+                graphic = Grass
             end
+
+            local level = {
+                level = i,
+                x = dx,
+                y = dy,
+                image = graphic
+            }
+
+            table.insert(levels, level)
 
             local mouseCoorsX, mouseCoorsY = Cam:toWorld(mouseX, mouseY)
 
@@ -38,23 +48,14 @@ function WorldMap:draw()
              then
                 function love.mousepressed(mx, my, button)
                     if button == 1 then
-                        if graphic.id == 1 then
-                            BackgroundImage = BackgroundImageGrass
-                        elseif graphic.id == 2 then
-                            BackgroundImage = BackgroundSand
-                        elseif graphic.id == 3 then
-                            BackgroundImage = BackgroundRocks
-                        else
-                            BackgroundImage = BackgroundDirt
-                        end
-                        -- Change to game view
+                        BackgroundImage = BackgroundSand
                         GameState = 1
                     end
                 end
 
-                Lg.draw(graphic.image, dx, dy - 10, nil, .5)
+                Lg.draw(level.image, dx, dy - 10, nil, .5)
             else
-                Lg.draw(graphic.image, dx, dy, nil, .5)
+                Lg.draw(level.image, dx, dy, nil, .5)
             end
         end
     end
