b856672bfd7274c444f9b623e9256d7ac61e195d
oscar lodriguez <o.lodriguez@gmail.com>
2020-01-17 22:15:12 +0100

    forest map

--- src/Worldmap.lua
+++ src/Worldmap.lua
@@ -3,6 +3,7 @@ local levels = {}
 local circleSize = 20
 
 function WorldMap:draw()
+    local mouseX, mouseY = Lm.getPosition()
     Lg.draw(WorldMapBackground, 0, 0)
 
     -- Extract to own file later
@@ -17,27 +18,31 @@ function WorldMap:draw()
 
     self:injectLevel(
         {
-            location = {x = Lg.getWidth() / 2 + 400, y = Lg.getWidth() / 2 - 400},
+            location = {x = util.getCenter(Lg.getWidth()) + 400, y = util.getCenter(Lg.getWidth()) - 400},
             player = {},
             predators = {},
             rivals = {}
         }
     )
 
+    local mouseCoorsX, mouseCoorsY = Cam:toWorld(mouseX, mouseY)
+
     for i, level in ipairs(levels) do
-        Lg.circle("fill", level.location.x, level.location.y, circleSize, 20)
+        local inBetween =
+            mouseCoorsX >= level.location.x and mouseCoorsY >= level.location.y and
+            mouseCoorsX < level.location.x + circleSize and
+            mouseCoorsY < level.location.y + circleSize
+
+        if inBetween then
+            Lg.circle("fill", level.location.x, level.location.y, circleSize + 5, 20)
+        else
+            Lg.circle("fill", level.location.x, level.location.y, circleSize, 20)
+        end
 
         function love.mousepressed(x, y, button)
-            local inBetweenWidth = x > level.location.x and x < level.location.x / 2
-            local inBetweenHeight = y > level.location.y and y < level.location.y / 2
-            print("pressed before", x, level.location.x)
-
-            if button == 1 and inBetweenWidth and inBetweenHeight then
-                print("pressed")
+            if button == 1 then
                 GameState = 1
                 BackgroundImage = BackgroundRocks
-            else
-                return
             end
         end
     end
